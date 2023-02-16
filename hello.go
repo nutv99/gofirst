1935541a911444b7637cd236c4cb2b64f809e8c2
nutv99 <nutv99@gmail.com>
2023-02-16 10:42:27 +0700

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
