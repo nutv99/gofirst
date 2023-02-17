82512f67d1d27dfb71458a06e1866e7664eb1f9c
nutv99 <nutv99@gmail.com>
2023-02-17 17:26:01 +0700

    Update index.go

--- api/index.go
+++ api/index.go
@@ -3,12 +3,27 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	
+	"database/sql"
+        "github.com/go-sql-driver/mysql"
 
 	. "github.com/tbxark/g4vercel"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	server := New()
+	
+	connectDB := 0 
+	 db, err := sql.Open("mysql", "nn9iehurcse16wz1goup:pscale_pw_3Kcm1dFxnmkdImRfFRXPnuygPQYfhE7jUckHW49pr4n@tcp(ap-southeast.connect.psdb.cloud)/it_asset")
+    if err != nil {
+	connectDB = -1
+        panic(err.Error())	    
+    } else {
+	connectDB = 1    
+    }
+    defer db.Close()
+	
+	
 
 	server.GET("/", func(context *Context) {
 		context.JSON(200, H{
