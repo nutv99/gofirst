eb3d27ba518b63471a5bcfd2651d0c99600851df
nutv99 <nutv99@gmail.com>
2023-02-17 19:20:41 +0700

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,31 +1,19 @@
+
 package handler
 
 import (
 	"fmt"
-	
-	
-	
-       ."github.com/tbxark/g4vercel"
-)
+	"net/http"
 
+	. "github.com/tbxark/g4vercel"
+)
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	server := New()
-	
-	//GOLANGMYSQL_DNS="lbg5pjees347lrun2wdl:pscale_pw_CgPWWfYLYTy3ziSn28WizLQ3fpt3dTTU24kgX82qxNA@tcp(ap-southeast.connect.psdb.cloud)/it_asset?tls=true"
-	//Db, err = gorm.Open(mysql.Open(GOLANGMYSQL_DNS), &gorm.Config{})
-	//if err != nil {
-	//	panic("failed to connect database")
-	//}
-	
-	
-	 
-	
-	
 
 	server.GET("/", func(context *Context) {
 		context.JSON(200, H{
-			"message": "hello go from vercel by nutv99 !!!!",
+			"message": "hello go from vercel !!!!",
 		})
 	})
 	server.GET("/hello", func(context *Context) {
