33f21f646b81f2e7f3788dded61f213bed6517f1
nutv99 <nutv99@gmail.com>
2023-02-18 10:46:21 +0700

    Update index.go

--- api/index.go
+++ api/index.go
@@ -4,39 +4,19 @@ package handler
 import (
 	"fmt"
 	"net/http"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 
 	. "github.com/tbxark/g4vercel"
 )
-var Db *gorm.DB
-var err error
-
-type User struct {
-	gorm.Model
-	UserName string
-	Password string
-	FullName string
-	Avatar   string
-}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	server := New()
-	
-	GOLANGMYSQL_DNS :="lbg5pjees347lrun2wdl:pscale_pw_CgPWWfYLYTy3ziSn28WizLQ3fpt3dTTU24kgX82qxNA@tcp(ap-southeast.connect.psdb.cloud)/it_asset?tls=true"
-	dsn := GOLANGMYSQL_DNS
-	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-	Db.AutoMigrate(&User{})
 
 	server.GET("/", func(context *Context) {
 		context.JSON(200, H{
 			"message": "hello go from vercel !!!!",
 		})
 	})
-	server.GET("/hello888", func(context *Context) {
+	server.GET("/hello", func(context *Context) {
 		name := context.Query("name")
 		if name == "" {
 			context.JSON(400, H{
