972068fd2433c64f8eba1c43696997f492346b77
nutv99 <nutv99@gmail.com>
2023-02-19 07:00:58 +0700

    Delete src directory

--- src/util.js
+++
@@ -1,9 +0,0 @@
-export const formatDate = (date = Date.now()) => {
-  const dtf = new Intl.DateTimeFormat('es', {
-    timeStyle: 'short',
-    hour12: true,
-    dateStyle: 'long',
-  })
-
-  return dtf.format(new Date(date))
-}
